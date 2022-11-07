270f969d549f4d79b34e3aaeb2fe42daffd727f9
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-11-07 20:20:44 -0300

    config, pages e packages iniciais

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import config from "../config.json";
+import styled from "styled-components";
+
+function HomePage() {
+    const homePageStyle = { backgroundColor: "red" };
+
+    return (
+        <div style={homePageStyle}>
+            <Menu />
+            <Header />
+            <TimeLine />
+        </div>
+    )
+}
+
+export default HomePage
+
+function Menu() {
+
+    return (
+        <div>
+            Menu
+        </div>
+    )
+}
+
+const StyledHeader = styled.div`
+    img{
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+`;
+function Header() {
+
+    return (
+        <StyledHeader>
+            <img src="banner" />
+            <img src={`https://github.com/${config.github}.png`} />
+            {config.name}
+            {config.job}
+        </StyledHeader>
+    )
+}
+
+function TimeLine() {
+
+    return (
+        <div>
+            TimeLine
+        </div>
+    )
+}
